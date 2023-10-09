a6afd709aeb1f3eb1ecef5e8755c4a117af91ba7
shubham@archstore.in-at-481412860284 <shubham@archstore.in>
2023-10-09 23:32:01 +0530

    feat(templates) : added front-end template

--- app.py
+++ app.py
@@ -14,6 +14,25 @@ def predict_datapoint():
 
         return render_template('predict.html',results="Good")
     
+@app.route("/tellsymptoms",methods=['GET', 'POST'])
+def tellsymptoms_datappoint():
+
+        return render_template('tellSymptoms.html', results="Good")
+
+@app.route("/possible-disease", methods=['GET', 'POST'])
+def possibledisease_datappoint():
+      
+        return render_template('possibleDisease.html', results="Good")
+
+@app.route("/suggestions", methods=['GET', 'POST'])
+def suggestions_datappoint():
+      
+        return render_template('suggestions.html', results="Good")
+
+@app.route("/faqs", methods=['GET', 'POST'])
+def faqs_datappoint(): 
+       
+        return render_template('faqs.html', results="Good")
 
 if __name__=="__main__":
     app.run(debug=True)       
