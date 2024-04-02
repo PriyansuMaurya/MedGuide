3e069036f8eee5613c6ba8a99389d46a8ddc8e2b
ShivamGupta <109647722+the-shivam-gupta@users.noreply.github.com>
2024-04-02 22:01:13 +0530

    feat: successfully updated the suggestion tab. (#15)
    * feat: successfully updated the suggestion tab.
    
    * Changed the styling of the cards.

--- medguide/extents.py
+++ medguide/extents.py
@@ -1,4 +1,5 @@
 from flask import Blueprint,render_template, request
+import json
 
 extents = Blueprint('extents',__name__)
 
@@ -6,14 +7,32 @@ extents = Blueprint('extents',__name__)
 
 @extents.route("/suggestions", methods=['GET', 'POST'])
 def suggestions():
+        # Open the JSON file
+        with open('/workspaces/MedGuide/medguide/templates/suggestion.json', 'r') as file:
+        # Load JSON data into a Python dictionary
+                disease_data = json.load(file)
         # clicked_button_name
         if request.method == 'POST':
-                disease = request.form.get('clicked_button_name')
+                disease = request.form.get('clicked_button_name')[2:-2]
+                def get_recommendations(my_disease):
+        # Dummy function to get recommendations based on disease
+                        recommendations = {}
                 
-                return render_template('suggestions.html', title="Suggestions", prescription = disease)
-                
-        # return f"{'shivam'}"
+                        # Get recommendations based on the provided disease
+                        if my_disease in disease_data:
+                                recommendations["prevention"] = disease_data[my_disease]["prevention"]
+                                recommendations["medication"] = disease_data[my_disease]["medication"]
+                                recommendations["overview"] = disease_data[my_disease]["overview"]
 
+                        else:
+                                recommendations["prevention"] = ["No specific prevention measures found."]
+                                recommendations["medication"] = ["No specific medications found."]
+                                recommendations["overview"] = ["No overview found."]
+
+                        return recommendations
+                                
+                return render_template('suggestions.html', title="Suggestions", prescription = get_recommendations(disease), disease_name=disease)
+                
 @extents.route("/faqs", methods=['GET', 'POST'])
 def faqs(): 
        
